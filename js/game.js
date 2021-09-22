1a378dbc0f58e3400ef1b482ee9aef3b8017d27d
Stuti08coder <76516771+Stuti08coder@users.noreply.github.com>
2021-09-22 11:43:04 +0530

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -56,14 +56,18 @@ class Game{
                      players[index - 1].y = y;
                        
                      if(index === player.index){
-                        fill("red") 
-                        textSize(20)
-                       //add code to display the player's name on the respective basket.
-                        
+                         
+                         fill("black");
+                         textSize(25);
+                         text(allPlayers[plr].name ,x-25,y+25);
+
                          
                      }
                     
-                      
+                         textSize(25);
+                         fill("white");
+                         text("Player 1 :" +allPlayers.player1.score,50,50);
+                        text("Player 2 :" + allPlayers.player2.score, 50, 100);
                  
                  }
                 
@@ -100,14 +104,15 @@ class Game{
                  }
                  
                   if (player.index !== null) {
-                    for (var i = 0; i < fruitGroup.length; i++) {
-                        if (fruitGroup.get(i).isTouching(players)) {
-                            fruitGroup.get(i).destroy();
-                         
-                            
-                        }
-                        
-                    }
+                      for (var i = 0; i < fruitGroup.length; i++) {
+                          if (fruitGroup.get(i).isTouching(players)) {
+                              fruitGroup.get(i).destroy();
+                              player.score =player.score+1;
+                              player.update();
+                              
+                          }
+                          
+                      }
                   }
                 
 
